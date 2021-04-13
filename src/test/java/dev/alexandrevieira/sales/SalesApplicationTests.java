0e9b64195c3a2e9271ad249c2dc7527fc20cb56b
alexandrer0x <alexandrer0x@hotmail.com>
2021-04-13 12:11:11 -0300

    First commit

---
+++ src/test/java/dev/alexandrevieira/sales/SalesApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.alexandrevieira.sales;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SalesApplicationTests {
+
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
